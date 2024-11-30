30a236f442386613db7900d7f57425148cf11e7f
rcwoshimao <mantoumiaoshen@gmail.com>
2024-11-30 00:52:05 -0800

    fixed eda

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -121,6 +121,12 @@ def clean_gmap_meta_data(df):
         )
     ]
 
+    ## Filter data within latitude/longitude bounds
+    lat_min, lat_max = 32.5, 42
+    long_min, long_max = -124.4, -114.13
+    df = df[(df["latitude"] >= lat_min) & (df["latitude"] <= lat_max) &
+            (df["longitude"] >= long_min) & (df["longitude"] <= long_max)]
+
     return df
 
 
@@ -145,20 +151,14 @@ def clean_review_data(df, meta_df):
     ## adding feature
     df = df.assign(has_rep=df["resp"].notna())
 
-    ## Filter data within latitude/longitude bounds
-    lat_min, lat_max = 32.5, 42
-    long_min, long_max = -124.4, -114.13
-    df = df[(df["latitude"] >= lat_min) & (df["latitude"] <= lat_max) &
-            (df["longitude"] >= long_min) & (df["longitude"] <= long_max)]
-
     ## Filter data with review_time(unix) before 2005
     before_2005_timestamp = 1107849600 * 1000  # Convert to milliseconds
     df = df[df["review_time(unix)"] >= before_2005_timestamp]
 
     # merging
+    #df = df.merge(meta_df, how='inner', on='gmap_id')
     df = df.merge(meta_df, how="inner", right_on="gmap_id", left_on="gmap_id")
     df = df.drop_duplicates(subset=["reviewer_id", "text", "gmap_id"])
-
 
     return df
 
@@ -178,19 +178,19 @@ def get_clean_review_data(url: str, meta_url: str, chunk_size=100000, export=Fal
 
     """
     ## Set up paths
-    base_path = Path.cwd()
+    base_path = Path.cwd().parent
     file_path = base_path / "data" / "california_clean_data.json.gz"
     meta_file_path = base_path / "data" / "california_clean_metadata.json.gz"
     
 
     ## Download data if not available
-    # if not file_path.exists():
-    #     print(f"Review file not found. Downloading...")
-    #     download_review_data(url, file_path)
+    if not file_path.exists():
+        print(f"Review file not found. Downloading...")
+        download_review_data(url, file_path)
 
-    # if not meta_file_path.exists():
-    #     print(f"Metadata file not found. Downloading...")
-    #     download_review_data(meta_url, meta_file_path)
+    if not meta_file_path.exists():
+        print(f"Metadata file not found. Downloading...")
+        download_review_data(meta_url, meta_file_path)
 
     ## Process meta data
     if meta_file_path.exists():
@@ -205,31 +205,39 @@ def get_clean_review_data(url: str, meta_url: str, chunk_size=100000, export=Fal
         )
         return
 
+    
     ## Process review data in chunks
     if file_path.exists():
         print(f"Processing review data from: {file_path}")
         results = []
+        total_chunks = 445
         num_rows = 0
 
         ## Extract, Transform, Load
         with pd.read_json(
             file_path, compression="gzip", lines=True, chunksize=chunk_size
         ) as reader:
-            ## batch processing
-            for i, chunk in enumerate(reader):
-                ## Clean and process each chunk
-                chunk = chunk.rename(
-                    columns={
-                        "user_id": "reviewer_id",
-                        "name": "reviewer_name",
-                        "time": "review_time(unix)",
-                    }
-                )
-                chunk = clean_review_data(chunk, metadata_df)
-                num_rows += 1
-                print(f"Processed and saved chunk {i}")
-                results.append(chunk)
+            ## batch processin
+            with tqdm(total=total_chunks, desc="Processing chunks") as pbar:
+                for i, chunk in enumerate(reader):
+                    # Rename columns
+                    chunk = chunk.rename(
+                        columns={
+                            "user_id": "reviewer_id",
+                            "name": "reviewer_name",
+                            "time": "review_time(unix)",
+                        }
+                    )
+                    
+                    # Clean the chunk
+                    chunk = clean_review_data(chunk, metadata_df)
+                    num_rows += len(chunk)
+                    results.append(chunk)
 
+                    # Update progress bar
+                    pbar.update(1)
+                    pbar.set_postfix({"Processed Rows": num_rows})
+                    
         ## Combine all processed chunks into a single DataFrame
         clean_reviews = pd.concat(results, ignore_index=True)
         print(f"Processed {num_rows} review entries.")
